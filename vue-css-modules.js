4c00c6a53526440f7d0f6710c472f3ff1c183902
Nathan Reid <nathan-code@nathantreid.com>
2017-03-02 18:24:15 -0600

    initial commit

---
+++ vue-css-modules.js
@@ -0,0 +1,38 @@
+global.vue = global.vue || {}
+global.vue.lang = global.vue.lang || {}
+
+function compileStyles({
+                         source,
+                         inputFile,
+                         dependencyManager,
+                         tag
+                       }) {
+  try {
+    const result = global.cssModules.compiler.compileFromSource(source, inputFile, {transpileCssModules: !!tag.attribs.module});
+    const compileResult = result.compileResult;
+    if (!!tag.attribs.module)
+    
+    let importsCode;
+    if (compileResult.imports) {
+      importsCode = compileResult.imports.map(importPath => `require ('${importPath}');`).join('\n');
+    }
+    if (result.referencedImportPaths) {
+      result.referencedImportPaths.forEach(importPath => dependencyManager.addDependency(importPath));
+    }
+
+    const vueResult = {
+      css: result.compileResult.stylesheet,
+      map: JSON.stringify(result.sourceMap),
+      cssModules: tag.attribs.module ? result.compileResult.tokens : null,
+      js: importsCode,
+    };
+    return vueResult;
+  } catch (err) {
+    console.error(err);
+    console.error(err.stack);
+    throw err;
+  }
+}
+
+global.vue.lang.scss = compileStyles;
+global.vue.lang.css = compileStyles;
