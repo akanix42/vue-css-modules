8d3bbeeaed87d38c6b5dcf4e95f6900f7752d143
Nathan Reid <nathan-code@nathantreid.com>
2017-03-18 23:58:35 -0500

    update metadata and bump to nathantreid:css-modules@2.5.1

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'nathantreid:vue-css-modules',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'CSS modules compiler for akryum:vue-component using nathantreid:css-modules',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/nathantreid/vue-css-modules',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -14,7 +14,7 @@ Package.registerBuildPlugin({
   name: 'scoped-name',
   use: [
     'ecmascript@0.5.8',
-    'nathantreid:css-modules@2.5.0'
+    'nathantreid:css-modules@2.5.1'
   ],
   sources: [
     'vue-css-modules.js'
