917423408bef4a1e856c54cab8aa7280f1a3df96
Nathan Reid <>
2018-01-24 00:07:34 -0600

    add error message in case Meteor isn't loading the nathantreid:css-modules package. In dev, sometimes it does and sometimes it doesn't...

--- vue-css-modules.js
+++ vue-css-modules.js
@@ -10,6 +10,19 @@ function compileStyles({
                          cssModules,
                        }) {
   try {
+    if (!global.cssModules) {
+      console.error('nathantreid:vue-css-modules sometimes requires the nathantreid:css-modules package to be installed. To add the package, run the following command:');
+      console.error('\nmeteor add nathantreid:css-modules\n');
+      console.error('By default, nathantreid:css-modules will process your .css files as well. To avoid this, add the following to your package.json:');
+      console.error(`
+        {
+          "cssModules": {
+            "extensions": ["dont-process-anything"],
+          }
+        }
+      `);
+      throw new Error("See above error message");
+    }
     const result = global.cssModules.compiler.compileFromSource(source, inputFile);
     const compileResult = result.compileResult;
 
