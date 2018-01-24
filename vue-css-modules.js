858e56196a337e1ba323ca919191bbdaab7e40b2
Nathan Reid <>
2018-01-24 00:06:50 -0600

    add support for non-existent styles warning

--- vue-css-modules.js
+++ vue-css-modules.js
@@ -6,7 +6,8 @@ function compileStyles({
                          source,
                          inputFile,
                          dependencyManager,
-                         tag
+                         tag,
+                         cssModules,
                        }) {
   try {
     const result = global.cssModules.compiler.compileFromSource(source, inputFile);
@@ -20,19 +21,24 @@ function compileStyles({
       compileResult.importTree.forEach(importPath => dependencyManager.addDependency(importPath));
     }
 
+    if (compileResult.stylesCode) {
+      const moduleName = typeof tag.attribs.module === 'string' ? tag.attribs.module : '$style';
+      if (cssModules === undefined) {
+        cssModules = {};
+      }
+      cssModules[moduleName] = compileResult.stylesCode;
+    }
+
     const vueResult = {
       css: result.compileResult.stylesheet,
       map: JSON.stringify(result.sourceMap),
-      cssModules: result.compileResult.tokens,
+      cssModules,
       js: importsCode,
     };
     return vueResult;
   } catch (err) {
     console.error(err);
-    console.error(err.stack);
+    // console.error(err.stack);
     throw err;
   }
 }
-
-// global.vue.lang.scss = compileStyles;
-// global.vue.lang.css = compileStyles;
